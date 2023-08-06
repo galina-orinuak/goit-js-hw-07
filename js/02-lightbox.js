ccf27cc2581f582410a4648010b993ba6ee3dd2a
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-06 19:32:04 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const container = document.querySelector(".gallery");
+
+
+function createMarkup(arr){
+    return arr.map(({preview, original, description}) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`).join('')
+};
+
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+container.addEventListener('click', handlerImgClick);
+function handlerImgClick(evt){
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return;}
+    
+    var lightbox = new SimpleLightbox('.gallery a', { 
+      captions: true,
+      captionType:'attr',
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+
+    });
+    
+    }
 
 console.log(galleryItems);
+
+
